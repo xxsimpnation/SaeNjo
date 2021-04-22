bb4c32282efd94febb29ed9a049b897d80800067
Nisa Zahir <nisa24.bz@student.uns.ac.id>
2021-04-22 12:19:16 +0700

    Nambah halaman 3

---
+++ app/src/main/java/com/example/saenjo/Wakeup.kt
@@ -0,0 +1,11 @@
+package com.example.saenjo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Wakeup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wakeup)
+    }
+}
