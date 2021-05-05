3474d5a7d6740355ef92a9b2346d19e2725535cb
Nisa Zahir <nisa24.bz@student.uns.ac.id>
2021-05-05 08:44:44 +0700

    Nambah button setOnClickListener

--- app/src/main/java/com/example/saenjo/MainActivity.kt
+++ app/src/main/java/com/example/saenjo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.saenjo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val nextBtn: Button = findViewById(R.id.btMove)
+        nextBtn.setOnClickListener{
+            val intent = Intent(this, Name::class.java);
+            intent.putExtra("data", "test data")
+            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/example/saenjo/Name.kt
+++ app/src/main/java/com/example/saenjo/Name.kt
@@ -1,11 +1,20 @@
 package com.example.saenjo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Name : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+
+        val nextBtn: Button = findViewById(R.id.btNama)
+        nextBtn.setOnClickListener{
+            val intent = Intent(this, Wakeup::class.java);
+            intent.putExtra("data", "test data")
+            startActivity(intent)
+        }
     }
 }
