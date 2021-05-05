88f6a0bfb7b4a693ae5e710ec24de8e502ed6d00
Niko Putra <nikolatief11@gmail.com>
2021-05-05 13:47:34 +0700

    nambah button to notif

--- app/src/main/java/com/example/saenjo/Wakeup.kt
+++ app/src/main/java/com/example/saenjo/Wakeup.kt
@@ -1,11 +1,20 @@
 package com.example.saenjo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Wakeup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wakeup)
+
+        val nextBtn: Button = findViewById(R.id.btBangun)
+        nextBtn.setOnClickListener{
+            val intent = Intent(this, notification::class.java);
+            intent.putExtra("data", "test data")
+            startActivity(intent)
+        }
     }
 }
