84fb65019db2c0c49e04b637a1ddcc8a724ca7e2
Niko Putra <nikolatief11@gmail.com>
2021-05-10 01:43:35 +0700

    numberpicker udah ditambah

--- app/src/main/java/com/example/saenjo/Wakeup.kt
+++ app/src/main/java/com/example/saenjo/Wakeup.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.Toast
 
 class Wakeup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,39 @@ class Wakeup : AppCompatActivity() {
             intent.putExtra("data", "test data")
             startActivity(intent)
         }
+
+        val numPickerHr: NumberPicker = findViewById(R.id.numPickerHr)
+        numPickerHr.maxValue = 12
+        numPickerHr.minValue = 0
+
+        numPickerHr.setOnClickListener {
+            Toast.makeText(this, numPickerHr.value.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        val numPickerMin: NumberPicker = findViewById(R.id.numPickerMin)
+        numPickerMin.maxValue = 59
+        numPickerMin.minValue = 0
+
+        numPickerHr.setOnClickListener {
+            Toast.makeText(this, numPickerMin.value.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        val colors = arrayOf("AM", "PM")
+        val numPickerAm: NumberPicker = findViewById(R.id.numPickerAm)
+        numPickerAm.minValue = 0
+        numPickerAm.maxValue = colors.size - 1
+
+
+        // Set the valued to be displayed in number picker
+        numPickerAm.displayedValues = colors
+
+
+        // Set number picker value change listener
+        numPickerAm.setOnClickListener {
+            Toast.makeText(
+                    this, "${colors[numPickerAm.value]}", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }
