9627f4659f171a0d51368ea9de427b3e5001f589
xxsimpnaton git <nikolatief11@gmail.com>
2021-06-23 16:08:38 +0700

    Update drink,kt

--- app/src/main/java/com/example/saenjo/Drink.kt
+++ app/src/main/java/com/example/saenjo/Drink.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -13,12 +14,6 @@ class Drink : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink)
 
-        val askInput : LinearLayout = findViewById(R.id.ask_input_drink)
-        val inputDrink : LinearLayout = findViewById(R.id.input_drink)
-        val toLayoutInput: ImageButton = findViewById(R.id.to_layout_input)
-        toLayoutInput.setOnClickListener {
-            askInput.visibility = View.INVISIBLE
-            inputDrink.visibility = View.VISIBLE
-        }
+
     }
 }
